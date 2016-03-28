70e85c2e8525fb4ad8562cb8780df64488769dd9
ErinUC <erinuc@gmail.com>
2016-03-27 22:04:58 -0400

    updated production.rb file paperclip config to include AWS S3 hostname

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326215706) do
+ActiveRecord::Schema.define(version: 20160327185145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20160326215706) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  create_table "keywords", force: :cascade do |t|
+    t.string   "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "comments", "articles"
 end
