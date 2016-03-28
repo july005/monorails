d4444ed54d48ad3d66ecf0890cf497b4358c4356
ErinUC <erinuc@gmail.com>
2016-03-27 20:20:06 -0400

    Redid bootstrap theme

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
 
    def index
-    articles = Article.order('created_at DESC').limit(3)
-    @articles = Article.order('created_at DESC').limit(3)
+    articles = Article.order('created_at DESC').limit(1)
+    @articles = Article.order('created_at DESC').limit(1)
   end
 end
