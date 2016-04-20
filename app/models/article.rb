cc5525ff0b781518e50937da66dfc680f4d3c82a
ErinUC <erinuc@gmail.com>
2016-04-20 14:24:24 -0400

    Removed intro (and background image for intro), made only 1 post (most recent) display in the welcome_controller.rb, changed image sizes in articles model, labeled the old index as _welcomeindex_ol.html.erb

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ require 'aws/s3'
 
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  has_attached_file :image, styles: { large: "900x900>", medium: "500x500>", thumb: "250x250>" }
+  has_attached_file :image, styles: { large: "900x900>", medium: "700x700>", thumb: "250x250>" }
   belongs_to :user
   validates :title, presence: true,
                     length: { minimum: 5 }
